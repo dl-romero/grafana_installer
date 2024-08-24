287632a0900214ee9ce8f0117664290ee36509a6
David Romero <48596887+dl-romero@users.noreply.github.com>
2024-08-24 09:29:46 -0700

    Create install_grafana.sh

---
+++ install_grafana.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /tmp
+wget -q -O gpg.key https://rpm.grafana.com/gpg.key
+sudo rpm --import gpg.key
+sudo cp grafana.repo /etc/yum.repos.d/
+sudo dnf install -y grafana
+sudo systemctl daemon-reload
+sudo systemctl start grafana-server
+sudo systemctl status grafana-server
+sudo systemctl enable grafana-server.service
